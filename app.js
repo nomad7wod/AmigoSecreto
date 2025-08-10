2487da8ebca7a2b78c4f97330080d6802070798e
hernan.marecos <hernan.marecos@qbitla.com>
2025-08-10 01:32:20 -0500

    Funciones de actualizar lista y de sorteo

--- app.js
+++ app.js
@@ -11,4 +11,32 @@ function agregarAmigo() {
 
     amigos.push(nombre);
     input.value = "";
+    actualizarLista();
+}
+
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
 }
